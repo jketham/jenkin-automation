d0a709c71f6ab870411d130a8c6d5c3ba78a1a9a
jketham <jketham@gmail.com>
2023-10-04 20:16:04 +0530

    added

---
+++ src/main/java/com/example/jenkinautomation/JenkinAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinautomation/JenkinAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
